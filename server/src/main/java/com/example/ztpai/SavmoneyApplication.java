dd7700e2ef74abe45edca589adaa1c7f85d12b14
kamil <kam.flis99@gmail.com>
2021-03-09 01:11:31 +0100

    Initial commit

---
+++ server/src/main/java/com/example/ztpai/SavmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ztpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavmoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavmoneyApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/example/ztpai/SavmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ztpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavmoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
